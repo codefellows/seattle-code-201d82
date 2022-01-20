ac0526096967f1bfa24667259b829452590926f2
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2022-01-20 12:00:36 -0800

    inclass-demo code

--- class-09/inclass-demo-form-events/js/app.js
+++ class-09/inclass-demo-form-events/js/app.js
@@ -1,32 +1,56 @@
 'use strict';
 
+// Step 1: Grab the element that we want to listen to
 let myContainer = document.getElementById('container');
 let parentEl = document.getElementById('results');
 
+let myForm = document.getElementById('my-form');
 
-function handleClick(event) {
-  console.log('this is the event', event);
-  console.log('this is the target', event.target);
+// Step 3: Define our event handler - callback function
+// function handleClick(event) {
+//   console.log('this is the event', event);
+//   console.log('this is the target', event.target);
 
-  if (event.target.id === 'box-one') {
-    let pEl = document.createElement('p');
-    pEl.textContent = 'Box 1 was clicked!';
-    parentEl.append(pEl);
-  } else if (event.target.id === 'box-two') {
-    let pEl = document.createElement('p');
-    pEl.textContent = 'Box 2 was clicked!';
-    parentEl.append(pEl);
-  } else if (event.target.id === 'box-three') {
-    let pEl = document.createElement('p');
-    pEl.textContent = 'Box 3 was clicked!';
-    parentEl.append(pEl);
-  } else {
-    let pEl = document.createElement('p');
-    pEl.textContent = 'CLICK THE BOXES ONLY!';
-    parentEl.append(pEl);
-  }
+//   if (event.target.id === 'box-one') {
+//     let pEl = document.createElement('p');
+//     pEl.textContent = 'Box 1 was clicked!';
+//     parentEl.append(pEl);
+//   } else if (event.target.id === 'box-two') {
+//     let pEl = document.createElement('p');
+//     pEl.textContent = 'Box 2 was clicked!';
+//     parentEl.append(pEl);
+//   } else if (event.target.id === 'box-three') {
+//     let pEl = document.createElement('p');
+//     pEl.textContent = 'Box 3 was clicked!';
+//     parentEl.append(pEl);
+//   } else {
+//     let pEl = document.createElement('p');
+//     pEl.textContent = 'CLICK THE BOXES ONLY!';
+//     parentEl.append(pEl);
+//   }
+// }
+
+// Step 3 - EVENT HANDLER - CALLBACK FUNCTION
+
+function handleSubmit(event){
+  event.preventDefault();
+  console.dir(event.target);
+
+  let name = event.target.firstName.value;
+  console.log('name', name);
+
+  let age = +event.target.age.value;
+  console.log(typeof age);
+  
+  let housewives = event.target.housewives.value;
+  console.log('housewive', housewives);
 }
 
-myContainer.addEventListener('click', handleClick);
+
+
+// Step 2: Add our eventlistener - 2 args (event, callback function)
+// myContainer.addEventListener('click', handleClick);
+
+myForm.addEventListener('submit', handleSubmit);
 
 // form
